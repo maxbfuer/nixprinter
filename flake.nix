b825ba6673348fa7acae1179841174e710908bb1
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-06-17 12:41:54 -0700

    remove deploy-rs code

--- flake.nix
+++ flake.nix
@@ -1,33 +1,17 @@
 {
   description = "Deployment for my Klipper-based 3D printing system.";
 
-  # For accessing `deploy-rs`'s utility Nix functions
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
-    deploy-rs,
   }: {
     nixosConfigurations.nixprinter = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [./configuration.nix];
     };
-
-    deploy.nodes.nixprinter = {
-      hostname = "192.168.0.25";
-      sshUser = "root";
-      profiles.system = {
-        user = "root";
-        path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.nixprinter;
-      };
-    };
-
-    # This is highly advised, and will prevent many possible mistakes
-    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   };
 }
