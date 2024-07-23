7e628ead9d4fc146117f772fc0b9b632bf82bd8a
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-07-23 11:42:10 -0700

    add ustreamer systemd service

--- camera.nix
+++ camera.nix
@@ -8,4 +8,18 @@
   environment.systemPackages = with pkgs; [
     ustreamer
   ];
+
+  systemd.services.ustreamer = {
+    description = "Start the ustreamer web camera feed.";
+
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
+
+    # TODO: look into pi hardwaere encoding (m2m image?)
+    serviceConfig = {
+      Type = "simple";
+      User = "root";
+      ExecStart = ''${pkgs.ustreamer}/bin/ustreamer --host=0.0.0.0 --format=uyvy --workers=3 --persistent --drop-same-frames=30'';
+    };
+  };
 }
